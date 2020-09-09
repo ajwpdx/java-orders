5dc057fe4691a01817231801e690e678905829e0
Alex Williams <alexjwilliams1@gmail.com>
2020-09-09 14:43:05 -0700

    post request working

--- crudyorders/src/main/java/com/lambdaschool/crudyorders/controllers/CustomerController.java
+++ crudyorders/src/main/java/com/lambdaschool/crudyorders/controllers/CustomerController.java
@@ -5,13 +5,14 @@ import com.lambdaschool.crudyorders.models.Order;
 import com.lambdaschool.crudyorders.services.CustomerServices;
 import com.lambdaschool.crudyorders.views.OrderCounts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -54,4 +55,15 @@ public class CustomerController
         return new ResponseEntity<>(rtnList, HttpStatus.OK);
     }
 
+    // POST http://localhost:2019/customers/customer/
+    @PostMapping(value = "/customer", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> addCustomer(@Valid @RequestBody Customer newCustomer)
+    {
+        newCustomer.setCustcode(0);
+        newCustomer = customerServices.save(newCustomer); //returning the id
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{custcode}").buildAndExpand(newCustomer.getCustcode()).toUri();
+        responseHeaders.setLocation(newCustomerURI);
+        return new ResponseEntity<>(newCustomer, responseHeaders, HttpStatus.CREATED); // data, header, status
+    }
 }
--- crudyorders/src/main/java/com/lambdaschool/crudyorders/services/CustomerServicesImpl.java
+++ crudyorders/src/main/java/com/lambdaschool/crudyorders/services/CustomerServicesImpl.java
@@ -3,7 +3,9 @@ package com.lambdaschool.crudyorders.services;
 
 import com.lambdaschool.crudyorders.models.Customer;
 
+import com.lambdaschool.crudyorders.models.Order;
 import com.lambdaschool.crudyorders.repositories.CustomersRepository;
+import com.lambdaschool.crudyorders.repositories.OrdersRepository;
 import com.lambdaschool.crudyorders.views.OrderCounts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,9 @@ public class CustomerServicesImpl implements CustomerServices {
 
     @Autowired
     CustomersRepository customerrepos;
+
+    @Autowired
+    OrdersRepository orderrepos;
 
     @Override
     public List<Customer> findAllCustomers() {
@@ -45,7 +50,40 @@ public class CustomerServicesImpl implements CustomerServices {
         return null;
     }
 
+
     @Transactional
     @Override
-    public Customer save(Customer customer) { return customerrepos.save(customer);}
+    public Customer save(Customer customer)
+    {
+        Customer newCustomer = new Customer();
+
+        if(customer.getCustcode() != 0)
+        {
+            findCustomerById(customer.getCustcode());
+            newCustomer.setCustcode(customer.getCustcode());
+        }
+
+        //single value fields
+        newCustomer.setCustname(customer.getCustname());
+        newCustomer.setCustcity(customer.getCustcity());
+        newCustomer.setWorkingarea(customer.getWorkingarea());
+        newCustomer.setCustcountry(customer.getCustcountry());
+        newCustomer.setGrade(customer.getGrade());
+        newCustomer.setOpeningamt(customer.getOpeningamt());
+        newCustomer.setReceiveamt(customer.getReceiveamt());
+        newCustomer.setPaymentamt(customer.getPaymentamt());
+        newCustomer.setOutstandingamt(customer.getOutstandingamt());
+        newCustomer.setPhone(customer.getPhone());
+        newCustomer.setAgent(customer.getAgent());
+
+        //collections
+        newCustomer.getOrders().clear();
+        for (Order o : customer.getOrders())
+        {
+            Order newOrder = orderrepos.findById(o.getOrdnum())
+                    .orElseThrow(() -> new EntityNotFoundException("Order " + o.getOrdnum() + " Not Found!"));
+        }
+
+        return customerrepos.save(newCustomer);
+    }
 }
